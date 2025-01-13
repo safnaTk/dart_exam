8539161ddfc9cca658e8dcd99198703058502f80
safnaTk <safnatk697@gmail.com>
2025-01-13 12:50:16 +0530

    "Added main function to bin/dart_exam.dart"

--- bin/dart_exam.dart
+++ bin/dart_exam.dart
@@ -1,1 +1,71 @@
+// ignore_for_file: unused_local_variable, unused_element
 
+import 'dart:io';
+// import 'dart:svg';
+
+void main(){
+//  List <int> numbers;[12, 45, 7, 89, 23];
+
+//  print(findLargest(numbers));
+
+// section A
+
+ //1.
+
+//  Map<String,dynamic> StudentDetails={
+//   "name":"john",
+//   "age" :30,
+//   "isstudent" :false,
+//  };
+  
+//    for (var key in StudentDetails.keys) 
+//    {
+//     print("$key: ${StudentDetails[key]}");
+//    }
+
+ 
+
+  //2.
+//   for (i=0;i<=20;i++) {
+//     if (i % 2 ==0) {
+//       print(i);
+// }
+//   }
+
+//3. 
+
+// List<int> numbers =[10,20,30,40,50];
+// int sum=0;
+// for(int number in numbers){
+//   sum=sum+number;
+// }
+// print("sum of all numbers is $sum");
+
+//4.
+
+//5.
+// int findLargest(List<int>numbers){
+//   int larger =numbers[0];
+//   for(int number in numbers){
+//     if(number>larger){
+//       larger=number;
+//     }
+//   }
+//   return larger;
+//     }
+
+  
+
+
+
+// 6.
+
+// int rows=4; 
+//   for(int i=0;i<=rows;i++){
+//     for(int j=0;j<=i;j++){
+//     stdout.write("$i ");  
+//     }
+//     print(" ");
+//   }
+  
+}
