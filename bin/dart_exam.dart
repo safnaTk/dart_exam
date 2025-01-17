6cccb3602f64136a1c939d6ab1dabec36ad7c3bd
safnaTk <safnatk697@gmail.com>
2025-01-17 23:53:29 +0530

    add new questions

--- bin/dart_exam.dart
+++ bin/dart_exam.dart
@@ -1,32 +1,32 @@
-// ignore_for_file: unused_local_variable, unused_element
+
+
+//  import 'dart:io';
+
+
 
 import 'dart:io';
 
-// import 'dart:svg';
+// import 'package:test/test.dart';
 
 void main(){
-//  List <int> numbers;[12, 45, 7, 89, 23];
-  
-//  print(findLargest(numbers));
+//   List<int> numbers = [12, 45, 7, 89, 23];
+//   print("Largest number: ${findLargest(numbers)}");  
+
+ 
 
 // section A
 
  //1.
 
-//  Map<String,dynamic> StudentDetails={
+//  Map<String,dynamic> studentDetails={
 //   "name":"john",
 //   "age" :30,
 //   "isstudent" :false,
 //  };
-  
-//    for (var key in StudentDetails.keys) 
-//    {
-//     print("$key: ${StudentDetails[key]}"); //value access
-    
  
-// }
-
-  //2.
+//   print(studentDetails);
+ 
+//2.
 
   // for (int i = 1; i <= 20; i++) {
   //   if (i % 2 == 0) {
@@ -36,21 +36,18 @@ void main(){
 
 //3. 
 
-// List<int> numbers =[10,20,30,40,50];
+// List<int> numbers =[10,20,30,40,50];   
 // int sum=0;
-// for(int number in numbers){
-//   sum=sum+number;
+// for(int i=0;i<numbers.length;i++){        
+//   sum=sum+numbers[i];
 // }
 // print("sum of all numbers is $sum");
 
 //4.
 
-// stdout.write
-// ("Enter a number:");
-//   int number = int.parse(stdin.readLineSync()!);
-
-//   // Check the divisibility conditions and print the corresponding output
-//   if (number % 3 == 0 && number % 5 == 0) {
+// stdout.write("Enter a number:");
+//  int number = int.parse(stdin.readLineSync()!); 
+//  if (number % 3 == 0 && number % 5 == 0) {
 //     print("FizzBuzz");
 //   } else if (number % 3 == 0) {
 //     print('Fizz');
@@ -62,27 +59,160 @@ void main(){
 
 
 //5.
+
 // int findLargest(List<int>numbers){
-//   int larger =numbers[0];
-//   for(int number in numbers){
-//     if(number>larger){
-//       larger=number;
+//   int largest =numbers[0];
+//   for(int i=1;i<numbers.length;i++){
+//     if(numbers[i]>largest){
+//       largest=numbers[i];
 //     }
 //   }
-//   return larger;
-//     }
+//   return largest;
+// }
 
-  
 
 // 6.
 
-int rows=4; 
-  for(int i=1;i<=rows;i++){
-    for(int j=1;j<=i;j++){
-    stdout.write("$i ");  
-    }
-    print(" ");
-  }
+// int rows=4; 
+//   for(int i=1;i<=rows;i++){
+//     for(int j=1;j<=i;j++){
+//     stdout.write("$i ");  
+//     }
+//     print(" ");
+//   }
   
-}
+
+  // section B
+
+
+// 1. 
+
+  // int n=9;
+  //  for (int i = 0; i < n; i++){
+  //   for( int j=0;j<n;j++){
+  //     if (i == 0 || i == n - 1 || i + j == n - 1 || i == j  ){       
+  //     stdout.write("*");
+  //   }else{
+  //     stdout.write(" ");
+  //   }
+    
+  //   }
+  //   print("");
+  //  }
+  
+
+// 2.
+
+// List<int> numbers = [];
+//   stdout.write("Enter a number (0 to stop): ");
+//     int userinput = int.parse(stdin.readLineSync()!);
+//     for (int i = 0;i<userinput; i++){
+//   if (userinput == 0){
+//     break;
+//   } 
+   
+// numbers.add(userinput);
+   
+//  }
+//  List<int> evenNumbers = [];
+//   List<int> oddNumbers = [];
+
+//   for (int i = 0; i < numbers.length; i++) {
+//     if (numbers[i] % 2 == 0) {
+//       evenNumbers.add(numbers[i]);
+//     } else {
+//       oddNumbers.add(numbers[i]);
+//     }
+//   }  
+
+//   int greatestNumber = numbers[0];
+//   for (int i = 1; i < numbers.length; i++) {
+//     if (numbers[i] > greatestNumber) {
+//       greatestNumber = numbers[i];
+//     }
+//   } 
+//   print("Even Numbers: $evenNumbers");
+//   print("Odd Numbers: $oddNumbers");
+//   print("Greatest Number: $greatestNumber");
+
+// 3.
+
+//   List<Map>books =[];
+// print("welcome to library management system");
+//  for( ;;){
+ 
+// print("""choose an option :
+
+//   1. Add Book
+//   2. Edit Book
+//   3. Delete Book
+//   4. Get All Books   """);
+//  stdout.write(" enter your choice :");
+//  int ? input=int.tryParse(stdin.readLineSync()!);
+//  // output based on user selection
+//  if(input==1){
+//   Map newBook=addBook();
+//   books.add(newBook);
+//   print(books);
+//  }
+ 
+//  if (input==2)
+//  {
+//   editBook();
+//  }
+//  if(input==3){
+//   deleteBook;
+//  }
+//  if(input==4){
+//   getAllBooks(books);
+//  }
+//   stdout.write("Do you want to continue(yes/no):");
+//   String userChoice =stdin.readLineSync()!;
+//  if(userChoice.toLowerCase() == "no"){
+//   break;
+//  }
+//  }
+//  }
+//    Map addBook(){
+//   stdout.write(" enter your book name:");
+//   String bookName =stdin.readLineSync()!;
+//   stdout.write("Enter publishing year :");
+//   String year  = stdin.readLineSync()!;
+//   Map newBook ={"name" :bookName, "year":year};
+//   return newBook;
+
+// }
+//  Map editBook(){
+//   stdout.write(" enter your book name:");
+//   String bookName =stdin.readLineSync()!;
+//   stdout.write("Enter publishing year :");
+//   String year  = stdin.readLineSync()!;
+//   Map newBook ={"name" :bookName, "year":year};
+//   return newBook;
+
+// }
+// Map deleteBook(){
+//   stdout.write(" enter your book name:");
+//   String bookName =stdin.readLineSync()!;
+//   stdout.write("Enter publishing year :");
+//   String year  = stdin.readLineSync()!;
+//   Map newBook ={"name" :bookName, "year":year};
+//   return newBook;
+// }
+// void getAllBooks(List<Map>books){
+// for(int index =0;index<books.length;index++){
+//   print("books ${index+1} : /n name :${books[index]["name"]},${books[index]["year"]}");
+// }
+
+ }
+
+
+
+
+
+
+
+
+
+
 
