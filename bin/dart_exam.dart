c0418c7a8fb7e92d46781a2361ae82b2e5ac71e3
safnaTk <safnatk697@gmail.com>
2025-01-17 23:49:10 +0530

    add new questions

--- bin/dart_exam.dart
+++ bin/dart_exam.dart
@@ -1,11 +1,12 @@
 // ignore_for_file: unused_local_variable, unused_element
 
 import 'dart:io';
+
 // import 'dart:svg';
 
 void main(){
 //  List <int> numbers;[12, 45, 7, 89, 23];
-
+  
 //  print(findLargest(numbers));
 
 // section A
@@ -20,17 +21,18 @@ void main(){
   
 //    for (var key in StudentDetails.keys) 
 //    {
-//     print("$key: ${StudentDetails[key]}");
-//    }
-
+//     print("$key: ${StudentDetails[key]}"); //value access
+    
  
+// }
 
   //2.
-//   for (i=0;i<=20;i++) {
-//     if (i % 2 ==0) {
-//       print(i);
-// }
-//   }
+
+  // for (int i = 1; i <= 20; i++) {
+  //   if (i % 2 == 0) {
+  //     print(i);
+  //   }
+  // }
 
 //3. 
 
@@ -42,6 +44,22 @@ void main(){
 // print("sum of all numbers is $sum");
 
 //4.
+
+// stdout.write
+// ("Enter a number:");
+//   int number = int.parse(stdin.readLineSync()!);
+
+//   // Check the divisibility conditions and print the corresponding output
+//   if (number % 3 == 0 && number % 5 == 0) {
+//     print("FizzBuzz");
+//   } else if (number % 3 == 0) {
+//     print('Fizz');
+//   } else if (number % 5 == 0) {
+//     print("Buzz");
+//   } else {
+//     print(number);
+//   }
+
 
 //5.
 // int findLargest(List<int>numbers){
@@ -58,12 +76,13 @@ void main(){
 
 // 6.
 
-// int rows=4; 
-//   for(int i=0;i<=rows;i++){
-//     for(int j=0;j<=i;j++){
-//     stdout.write("$i ");  
-//     }
-//     print(" ");
-//   }
+int rows=4; 
+  for(int i=1;i<=rows;i++){
+    for(int j=1;j<=i;j++){
+    stdout.write("$i ");  
+    }
+    print(" ");
+  }
   
 }
+
